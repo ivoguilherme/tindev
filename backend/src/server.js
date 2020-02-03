890fc2d6548215960fb16f32e66310f33fa950d3
Ivo Guilherme <ivo.guiilherme@gmail.com>
2020-02-03 20:43:42 -0300

    Instalação do mongoose e configuração inicial do banco de dados

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const server = express()
+
+mongoose.connect('mongodb+srv://root:root@cluster0-fqff3.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 server.use(express.json())
 server.use(routes)
